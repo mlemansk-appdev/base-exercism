a885049e2f204e61565c1460b9bc8aad60456185
mlemansk <mlemansk@chicagobooth.edu>
2021-04-09 23:46:49 +0000

    Completed Two_Fer

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -6,3 +6,12 @@ To get started with TDD, see the `README.md` file in your
 `ruby/two-fer` directory.
 =end
 
+class TwoFer
+  def TwoFer.two_fer(*args)
+    if args == []
+      message = "One for you, one for me."
+    else
+      message = "One for #{args.join()}, one for me."
+    end
+  end
+end
