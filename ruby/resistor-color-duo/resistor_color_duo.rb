91da545d8bb76fe026371d03091991ac4aec334e
mlemansk <mlemansk@chicagobooth.edu>
2021-04-15 01:51:32 +0000

    Solution working

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -6,3 +6,16 @@ To get started with TDD, see the `README.md` file in your
 `ruby/resistor-color-duo` directory.
 =end
 
+class ResistorColorDuo
+   
+    def self.value(array)
+        dictionary = {"black" => 0, "brown" => 1,"red" => 2,"orange" => 3,"yellow" => 4,"green" => 5,"blue" => 6,"violet" => 7,"grey" => 8,"white"=> 9}
+        color1 = array[0]
+        color2 = array[1]
+        first_digit = dictionary[color1].to_s
+        second_digit = dictionary[color2].to_s
+        text_answer =  first_digit + second_digit
+        number_answer = text_answer.to_i
+        return number_answer
+    end
+end
